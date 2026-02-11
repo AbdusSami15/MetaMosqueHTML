0c2fce9f8bfe66585e47c7fdfb44d635ba4d36c8
AbdusSami15 <abdus.sami@tecshield.io>
2026-02-11 17:32:53 +0500

    SAD

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 export default {
   root: ".",
+  // For GitHub Pages: if site is at user.github.io/RepoName/, set base: "/RepoName/"
+  base: "/",
   publicDir: "public",
   server: {
     port: 5173,
